330f9ecf5ea79af5784ff6862b1c85db75a091bb
rosanaramo <rosanaramo4@gmail.com>
2024-03-07 18:51:03 -0300

    first class

---
+++ playwrightgetstarted/src/main/java/App.java
@@ -0,0 +1,15 @@
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+
+public class App {
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
+    }
+}
