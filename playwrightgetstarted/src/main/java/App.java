3058035840e4231568bf28a8a32cc3092b7f8bb8
rosanaramo <rosanaramo4@gmail.com>
2024-03-10 18:58:42 -0300

    added class tests

--- playwrightgetstarted/src/main/java/App.java
+++ playwrightgetstarted/src/main/java/App.java
@@ -1,17 +1,17 @@
 import com.microsoft.playwright.*;
 import models.GetStartedPage;
-
-import java.nio.file.Paths;
+import tests.TestExample;
 
 public class App {
 
     public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
-            Page page = browser.newPage();
-            GetStartedPage getStartedPage = new GetStartedPage(page);
-            getStartedPage.navigate();
-            getStartedPage.clickGetStarted();
-        }
+//        try (Playwright playwright = Playwright.create()) {
+//            Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+//            Page page = browser.newPage();
+//            GetStartedPage getStartedPage = new GetStartedPage(page);
+//            TestExample tests = new TestExample(page);
+//            tests.validateButtonGetStarted();
+//            tests.validaDenovo();
+//        }
     }
 }
