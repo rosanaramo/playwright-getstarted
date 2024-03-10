7905e7bc1d3581a8536819d8f8c027fc03754d6f
rosanaramo <rosanaramo4@gmail.com>
2024-03-10 19:04:23 -0300

    refactoring

--- playwrightgetstarted/src/main/java/tests/TestExample.java
+++ playwrightgetstarted/src/main/java/tests/TestExample.java
@@ -3,44 +3,33 @@ package tests;
 import com.microsoft.playwright.*;
 import models.GetStartedPage;
 import org.junit.jupiter.api.*;
-
 import java.util.regex.Pattern;
-
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-
 public class TestExample {
 static Playwright playwright;
 static Browser browser;
-
 BrowserContext context;
 static Page page;
 static GetStartedPage getStartedPage;
-
-
-
 @BeforeAll
 static void launchBrowser(){
     playwright = Playwright.create();
     browser= playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 }
-
 @AfterAll
 static void closeBrowser(){
     playwright.close();
 }
-
 @BeforeEach
 void createContextAndPage(){
     context = browser.newContext();
     page = context.newPage();
     getStartedPage = new GetStartedPage(page);
 }
-
 @AfterEach
 void  closeContext(){
     context.close();
 }
-
 @Test
 void shouldValidateIntroductionPage(){
     getStartedPage.navigate();
